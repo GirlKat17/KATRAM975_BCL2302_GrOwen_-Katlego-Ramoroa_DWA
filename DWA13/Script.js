ccee8a2223bef07b24528dea8b18cba30f17f8ef
Katlego Ramoroa <ramoroakatlego@gmail.com>
2023-06-27 16:09:56 +0200

    updated

--- DWA13/Script.js
+++ DWA13/Script.js
@@ -20,41 +20,67 @@ const names = [
    'Frikkie'
   ];
 
-//  Use forEach to console log
-names.forEach(name => {
-  console.log(name);
-});
+// //  Use forEach to console log
+// names.forEach(name => {
+//   console.log(
+//     name,);
+// });
 
-//Use forEach to console log with  matching province
+// //Use forEach to console log with  matching province
 
-names.forEach((name, index) => {
-  console.log(`${name} (${provinces[index]})`);
-});
+// names.forEach((name, index) => {
+//   console.log(`${name} (${provinces[index]})`);
+// });
 
-//  Use map to convert province names to uppercase
+// //  Use map to convert province names to uppercase
+
+// const uppercaseProvinces = provinces.map(province => province.toUpperCase());
+// console.log(uppercaseProvinces);
+
+// //  Use map to create a new array with the amount of characters in each name
+
+// const nameLengths = names.map(name => name.length);
+// console.log(nameLengths);
+
+// //  Use sort to sort all provinces alphabetically
+
+// const sortedProvinces = provinces.sort();
+// console.log(sortedProvinces);
+
+// // Use filter to remove provinces with the word "Cape" and get the remaining count
+
+// const filteredProvinces = provinces.filter(province => !province.includes('Cape'));
+// const remainingProvinceCount = filteredProvinces.length;
+// console.log(remainingProvinceCount);
+
+// //  Use map and reduce to create an object indicating the province of each individual
+// const provinceObject = names.reduce((obj, name, index) => {
+//   obj[name] = provinces[index];
+//   return obj;
+// }, {});
+// console.log(provinceObject);
+const only = names.forEach(name => {});
+
+
+const mix = names.forEach((name, index) => {});
+
 
 const uppercaseProvinces = provinces.map(province => province.toUpperCase());
-console.log(uppercaseProvinces);
 
-//  Use map to create a new array with the amount of characters in each name
 
 const nameLengths = names.map(name => name.length);
-console.log(nameLengths);
 
-//  Use sort to sort all provinces alphabetically
 
 const sortedProvinces = provinces.sort();
-console.log(sortedProvinces);
 
-// Use filter to remove provinces with the word "Cape" and get the remaining count
 
 const filteredProvinces = provinces.filter(province => !province.includes('Cape'));
 const remainingProvinceCount = filteredProvinces.length;
-console.log(remainingProvinceCount);
 
-//  Use map and reduce to create an object indicating the province of each individual
+
 const provinceObject = names.reduce((obj, name, index) => {
   obj[name] = provinces[index];
   return obj;
 }, {});
-console.log(provinceObject);
+
+console.log(only,mix,uppercaseProvinces, nameLengths, sortedProvinces, remainingProvinceCount, provinceObject);
--- DWA13/Script2.js
+++ DWA13/Script2.js
@@ -15,22 +15,73 @@
 // //console.log(html,render); 
 
 
-
 const products = [
-    { product: 'banana', price: "2" },
-    { product: 'mango', price: 6 },
-    { product: 'potato', price: ' ' },
-    { product: 'avocado', price: "8" },
-    { product: 'coffee', price: 10 },
-    { product: 'tea', price: '' },
-  ]
+  { product: 'banana', price: "2" },
+  { product: 'mango', price: 6 },
+  { product: 'potato', price: ' ' },
+  { product: 'avocado', price: "8" },
+  { product: 'coffee', price: 10 },
+  { product: 'tea', price: '' },
+];
 
+// Use forEach to console.log each product name
+products.forEach(product => {
+  console.log(product.product);
+});
 
-  products.forEach(products => {
-    console.log(products);
+// Use filter to filter out products with names longer than 5 characters
+const filteredProducts = products.filter(product => product.product.length <= 5);
+console.log(filteredProducts);
 
+// Use filter and map to convert prices to numbers and remove products without prices,
+// then use reduce to calculate the combined price of remaining products
+const combinedPrice = products
+  .filter(product => product.price !== '' && !isNaN(Number(product.price)))
+  .map(product => Number(product.price))
+  .reduce((total, price) => total + price, 0);
+console.log(combinedPrice);
+
+// Use reduce to concatenate all product names into a string
+const productNames = products.reduce((str, product, index) => {
+  if (index === 0) {
+    return product.product;
+  } else if (index === products.length - 1) {
+    return str + ' and ' + product.product;
+  } else {
+    return str + ', ' + product.product;
+  }
+}, '');
+console.log(productNames);
+
+// Use reduce to find the highest and lowest-priced items
+const { highest, lowest } = products.reduce(
+  (result, product) => {
+    const price = Number(product.price);
+    if (isNaN(price)) {
+      return result;
+    }
+    if (price > result.highest.price) {
+      result.highest = { name: product.product, price };
+    }
+    if (price < result.lowest.price || !result.lowest.price) {
+      result.lowest = { name: product.product, price };
+    }
+    return result;
+  },
+  { highest: { name: '', price: -Infinity }, lowest: { name: '', price: Infinity } }
+);
+console.log(`Highest: ${highest.name}. Lowest: ${lowest.name}`);
+
+// Use Object.entries and reduce to recreate the object with modified keys
+const modifiedProducts = Object.entries(products).reduce((newArray, [key, value]) => {
+  newArray.push({
+    name: value.product,
+    cost: value.price,
   });
+  return newArray;
+}, []);
+console.log(modifiedProducts);
 
-  
+
 
  
